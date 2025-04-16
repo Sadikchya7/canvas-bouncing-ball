a072b7346df0df77dca4c2dc6f693ccd66df25d9
Sadiksha Maharjan <msadikchya@gmail.com>
2025-04-16 15:03:24 +0545

    collision with wall

--- js/index.js
+++ js/index.js
@@ -1,36 +1,57 @@
 var canvasDom = document.getElementById("main");
-canvasDom.height = 400;
+var ctx = canvasDom.getContext("2d");
 canvasDom.width = 800;
-canvasDom.style.backgroundColor = "red";
-var canvas = canvasDom.getContext("2d");
-let x = Math.random() * 2 + 2;
-let y = Math.random() * 2 + 2;
-let dirX = 1;
-let dirY = 1;
-let speed = 2;
-let radius = 40;
-let move = () => {
-  x += speed * dirX;
-  y += speed * dirY;
-  canvas.clearRect(0, 0, canvasDom.width, canvasDom.height);
-  canvas.beginPath();
-  canvas.arc(x, y, radius, 0, 360);
-  canvas.arc(x, y, radius, 0, 360);
-  canvas.fillStyle = "yellow";
-  canvas.fill();
+canvasDom.height = 500;
+canvasDom.style.backgroundColor = "black";
 
-  requestAnimationFrame(move);
-  if (x + radius >= canvasDom.width) {
-    dirX = -1;
-  }
-  if (y + radius >= canvasDom.height) {
-    dirY = -1;
-  }
-  if (x - radius <= 0) {
-    dirX = 1;
-  }
-  if (y - radius <= 0) {
-    dirY = 1;
+const balls = [];
+const ballList = 5;
+const ball = new Ball();
+
+for (let i = 0; i < ballList; i++) {
+  balls.push(new Ball());
+}
+
+moveBall = () => {
+  for (let i = 0; i < ballList; i++) {
+    const iball = balls[i];
+    iball.x += iball.dirx;
+    iball.y += iball.diry;
+
+    if (iball.x + iball.radius > canvasDom.width && iball.dirx > 0)
+      iball.dirx = -iball.dirx;
+    else if (iball.x - iball.radius < 0 && iball.dirx < 0)
+      iball.dirx = -iball.dirx;
+    if (iball.y + iball.radius > canvasDom.height && iball.diry > 0)
+      iball.diry = -iball.diry;
+    else if (iball.y - iball.radius < 0 && iball.diry < 0)
+      iball.diry = -iball.diry;
+
+    for (let j = i + 1; j < ballList; j++) {
+      let jball = balls[j];
+      const dirx = jball.x - iball.x;
+      const diry = jball.y - iball.y;
+      const distance = Math.sqrt(dirx * dirx + diry * diry);
+      const overLap = iball.radius + jball.radius - distance;
+      if (overLap > 0) {
+        // debugger;
+        // dirx *= -1;
+        // diry *= -1;
+      }
+    }
   }
 };
-move();
+function drawit() {
+  ctx.clearRect(0, 0, canvasDom.width, canvasDom.height);
+  for (let i = 0; i < ballList; i++) {
+    const manyBall = balls[i];
+    manyBall.drawBall(i);
+  }
+}
+animate = () => {
+  ctx.clearRect(0, 0, canvasDom.width, canvasDom.height);
+  drawit();
+  moveBall();
+  requestAnimationFrame(animate);
+};
+animate();
